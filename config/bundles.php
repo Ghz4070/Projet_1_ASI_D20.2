a8a213f71d7a7cbb9899869fab0dfa0b3ca3c008
RaphaelTo <paiva.raphaelt@gmail.com>
2019-02-13 10:38:39 +0100

    update some fixture, update method index in HomeController, update in template home/index.html.twig

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
