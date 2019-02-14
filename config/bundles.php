76b2ade545b67c7350554b40c2d2e8661e102a5e
RaphaelTo <paiva.raphaelt@gmail.com>
2019-02-14 15:29:52 +0100

    Add new bundle on composer KnpPaginator

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
