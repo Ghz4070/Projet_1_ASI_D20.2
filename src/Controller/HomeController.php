39408dd5091514a1f2054221204485816c433f24
Ghz4070 <ghezal-ilies@outlook.fr>
2019-02-15 11:12:11 +0100

    Moyenne et topDix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,32 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
-use App\Entity\User;
 use App\Entity\Vote;
-use App\Repository\ConferenceRepository;
 use App\Repository\VoteRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use mysql_xdevapi\Exception as ExceptionAlias;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Config\Definition\Exception\Exception;
-use Knp\Component\Pager\PaginatorInterface;
-use Doctrine\ORM\Tools\Pagination\Paginator;
+
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(VoteRepository $voteRepository, Request $request)
+    public function showConf(VoteRepository $voteRepository, Request $request)
     {
         $vote = $voteRepository->findAll();
 
-        $paginator  = $this->get('knp_paginator');
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $vote, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
@@ -43,33 +36,35 @@ class HomeController extends Controller
     /**
      * @Route("/conference/{id}", name="conferenceId")
      */
-    public function conferenceId(Conference $conference){
+    public function conferenceId(Conference $conference)
+    {
         return $this->render('home/ConferenceId.html.twig', [
             'conference' => $conference
         ]);
     }
+
     /**
      * @Route("/conference/{id}/{note}", name="vote")
      */
-    public function vote(Conference $conference,int $note, EntityManagerInterface $entityManager)
+    public function vote(Conference $conference, int $note, EntityManagerInterface $entityManager)
     {
-        if($this->getUser() !== null){
-            $userVote= $this->getUser()->getUserVote()->toArray();
-            foreach ($userVote as $value){
-                if($value->getConference()->getId() == $conference->getId()){
+        if ($this->getUser() !== null) {
+            $userVote = $this->getUser()->getUserVote()->toArray();
+            foreach ($userVote as $value) {
+                if ($value->getConference()->getId() == $conference->getId()) {
                     throw new Exception("User has already vote ");
                 }
             }
-                $votes = new Vote();
-                $votes->setUser($this->getUser());
-                $votes->setConference($conference);
-                $votes->setScore($note);
+            $votes = new Vote();
+            $votes->setUser($this->getUser());
+            $votes->setConference($conference);
+            $votes->setScore($note);
 
-                $entityManager->persist($votes);
-                $entityManager->flush();
+            $entityManager->persist($votes);
+            $entityManager->flush();
 
-                return $this->redirectToRoute('home');
-        } else{
+            return $this->redirectToRoute('home');
+        } else {
             throw new Exception("User not connected");
         }
 
